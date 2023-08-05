3479f38387fdcfb6321ead98b942353b28a490a4
k-prog55 <keianzai@keinoMacBook-Air.local>
2023-08-05 15:08:36 +0900

    セットアップ”

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// Shift を 2 回押して 'どこでも検索' ダイアログを開き、`show whitespaces` と入力して
+// Enter キーを押します。これでコードに空文字が表示されます。
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
